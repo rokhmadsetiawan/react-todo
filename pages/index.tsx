a4b1efb6e5c2c982e0bcae549f310f674507e90a
rse <rokhmad.setiawan@akarinti.tech>
2022-08-07 21:43:43 +0700

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import type { NextPage } from "next";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Link from "../src/Link";
+
+const Home: NextPage = () => {
+  return (
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          Hello World
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
+
+export default Home;
